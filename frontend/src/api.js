f89f9654d4a20f2a08f7c31b6ad5ddceaaeef4dd
Sam <samimahussain1103@gmail.com>
2026-09-01 13:30:05 +0530

    Delete frontend directory

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-export const API_BASE_URL = "http://127.0.0.1:8000";
-
-export const getAuthHeaders = () => {
-  const token = localStorage.getItem("accessToken");
-  return token
-    ? { Authorization: `Bearer ${token}` }
-    : {};
-};
